4e0b854901113c30cd6d31ef2f74438ea9a618df
Todd Yamakawa <toddy@google.com>
2021-09-30 19:54:19 +0000

    init.lua: Initial commit.

---
+++ init.lua
@@ -0,0 +1,13 @@
+
+-- Example
+-- https://github.com/shaunsingh/vimrc-dotfiles/blob/main/.config/nvim/init.lua
+local my_modules = {
+	"options",
+	"mappings",
+}
+for i = 1, #my_modules, 1 do
+	pcall(require, my_modules[i])
+end
+
+vim.cmd 'colorscheme desert'
+
