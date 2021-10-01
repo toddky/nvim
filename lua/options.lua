2939a558b12b7a0aa6fef28f772f416c86849409
Todd Yamakawa <toddy@google.com>
2021-10-01 19:11:24 +0000

    lua/options.lua: Updating.

--- lua/options.lua
+++ lua/options.lua
@@ -6,31 +6,49 @@ local opt = vim.opt
 -- Colorscheme
 execute('colorscheme jellybeans')
 
+
+-- =============================================================================
+-- OPTIONS
+-- =============================================================================
+
 -- Tab settings
 opt.expandtab      = false;
 opt.autoindent     = true;
 opt.tabstop        = 4;
-opt.shiftwidth     = 4;
+opt.shiftwidth     = 0;
+opt.softtabstop    = 0;
 
 -- List
 opt.list           = true
+opt.listchars      = { tab='▸ ', trail='·', precedes='←', extends='→' }
 
 -- Search
 opt.ignorecase     = true
 opt.incsearch      = true
 opt.hlsearch       = true
 
+opt.number         = true
 opt.relativenumber = true
 opt.backspace      = {"indent", "eol", "start"}
 opt.virtualedit    = "block";
 opt.encoding       = "utf-8";
 opt.viewoptions    = "folds,cursor,curdir,slash,unix";
 opt.sessionoptions = "curdir,help,tabpages,winsize";
+
 opt.clipboard      = "unnamedplus";
+
+-- Highlight currentl line
+opt.cursorline     = true
+--opt.cursorcolumn   = true
+-- TODO: Change color for insert mode
+--autocmd InsertEnter * highlight CursorLine guifg=something
+--autocmd InsertLeave * highlight CursorLine guifg=something
+
 
 -- Splits
 opt.splitbelow     = true;
 opt.splitright     = true;
+
 
 -- =============================================================================
 -- AUTOMATIC COMMANDS
