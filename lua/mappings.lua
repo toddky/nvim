bb0dbad31c1606dc8a53fbf43793b3fc9ce88fb4
Todd Yamakawa <toddy@google.com>
2021-10-05 16:26:41 +0000

    lua/mappings.lua: Using util.map.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,37 +1,5 @@
 
--- =============================================================================
--- FUNCTIONS
--- =============================================================================
-
-function map(mode, lhs, rhs, opts)
-	local options = {noremap = true, silent = true, unique = true}
-	if opts then options = vim.tbl_extend('force', options, opts) end
-	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
-function leader(lhs, rhs, opts)
-	map('n', '<space>' .. lhs, rhs, opts)
-end
-
-function nmap(lhs, rhs, opts)
-	map('n', lhs, rhs, opts)
-end
-
-function imap(lhs, rhs, opts)
-	map('i', lhs, rhs, opts)
-end
-
-function vmap(lhs, rhs, opts)
-	map('v', lhs, rhs, opts)
-end
-
-function xmap(lhs, rhs, opts)
-	map('x', lhs, rhs, opts)
-end
-
-function tmap(lhs, rhs, opts)
-	map('t', lhs, rhs, opts)
-end
+local util = require('util')
 
 
 -- =============================================================================
@@ -39,14 +7,20 @@ end
 -- =============================================================================
 
 -- Command
-nmap('<Enter>', ':')
+util.nmap('<Enter>', ':')
 
 -- Black hole registers
-nmap('c', '"_c')
-nmap('C', '"_C')
+util.nmap('c', '"_c')
+util.nmap('C', '"_C')
 
 -- Redo
-nmap('U', '<C-r>')
+util.nmap('U', '<C-r>')
+
+-- Indentation
+util.nmap('<Tab>', '>>')
+util.nmap('<S-Tab>', '<<')
+--util.xmap('<Tab>', '>>gv')
+--util.xmap('<S-Tab>', '<<gv')
 
 
 -- =============================================================================
@@ -54,65 +28,68 @@ nmap('U', '<C-r>')
 -- =============================================================================
 
 -- Beginning/end of line
-nmap('gh', '^')
-nmap('gl', '$')
-nmap('H', '^')
-nmap('L', '$')
+util.nmap('gh', '^')
+util.nmap('gl', '$')
+util.nmap('H', '^')
+util.nmap('L', '$')
 
 -- Center after search
-nmap('N', 'Nzz')
-nmap('n', 'nzz')
+util.nmap('N', 'Nzz')
+util.nmap('n', 'nzz')
 
 -- Mark with `M` and jump with `m`
-nmap('M', 'm')
-nmap('m', "'")
+util.nmap('M', 'm')
+util.nmap('m', "'")
 
 -- Repeat latest f, t, F or T in opposite direction
 -- Opposite direction of ;
-nmap(':', ',')
+util.nmap(':', ',')
 
 
 -- =============================================================================
 -- TABS
 -- =============================================================================
-nmap('>', '<cmd>tabnext<Enter>')
-nmap('<', '<cmd>tabprev<Enter>')
-leader('tn', '<cmd>tabnew %<Enter>')
-leader('td', '<cmd>tabclose<Enter>')
+util.nmap('>', '<cmd>tabnext<Enter>')
+util.nmap('<', '<cmd>tabprev<Enter>')
+util.leader('tn', '<cmd>tabnew %<Enter>')
+util.leader('td', '<cmd>tabclose<Enter>')
 
 
 -- =============================================================================
 -- WINDOWS
 -- =============================================================================
-leader('h', '<C-w>h')
-leader('j', '<C-w>j')
-leader('k', '<C-w>k')
-leader('l', '<C-w>l')
+util.leader('h', '<C-w>h')
+util.leader('j', '<C-w>j')
+util.leader('k', '<C-w>k')
+util.leader('l', '<C-w>l')
 
 
 -- =============================================================================
 -- BUFFERS
 -- =============================================================================
-leader('fq', '<cmd>q!<Enter>')
-leader('fw', '<cmd>w<Enter>')
-leader('fx', '<cmd>x<Enter>')
+util.leader('fj', '<cmd>bnext<Enter>')
+util.leader('fk', '<cmd>bprevious<Enter>')
+util.leader('fw', '<cmd>w<Enter>')
+util.leader('fx', '<cmd>x<Enter>')
+util.leader('fq', '<cmd>q!<Enter>')
 
 
 -- =============================================================================
 -- INSERT MODE
 -- =============================================================================
-imap('jj', '<esc>')
+util.imap('jj', '<esc>')
 
 
 -- =============================================================================
 -- VISUAL MODE
 -- =============================================================================
-vmap('v', '<C-v>')
-vmap('q', '<Esc>')
+util.vmap('v', '<C-v>')
+util.vmap('q', '<Esc>')
 
 
 -- =============================================================================
 -- TERMINAL MODE
 -- =============================================================================
-tmap('<Esc>', '<C-\\><C-n>')
+util.tmap('<Esc>', '<C-\\><C-n>')
+
 
