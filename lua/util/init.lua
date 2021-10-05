d0adbf6febb6077db80b277cb8b003f41481367a
Todd Yamakawa <toddy@google.com>
2021-10-05 16:46:56 +0000

    lua/util/init.lua: Updating local map function.

--- lua/util/init.lua
+++ lua/util/init.lua
@@ -1,38 +1,44 @@
 
+-- =============================================================================
+-- EXAMPLES
+-- =============================================================================
+-- https://github.com/folke/dot/blob/master/config/nvim/lua/util/init.lua
+
 local M = {}
+
 
 -- =============================================================================
 -- FUNCTIONS
 -- =============================================================================
 
-function M.map(mode, lhs, rhs, opts)
+local map = function(mode, lhs, rhs, opts)
 	local options = {noremap = true, silent = true, unique = true}
 	if opts then options = vim.tbl_extend('force', options, opts) end
 	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 function M.leader(lhs, rhs, opts)
-	M.map('n', '<space>' .. lhs, rhs, opts)
+	M.nmap('<space>' .. lhs, rhs, opts)
 end
 
 function M.nmap(lhs, rhs, opts)
-	M.map('n', lhs, rhs, opts)
+	map('n', lhs, rhs, opts)
 end
 
 function M.imap(lhs, rhs, opts)
-	M.map('i', lhs, rhs, opts)
+	map('i', lhs, rhs, opts)
 end
 
 function M.vmap(lhs, rhs, opts)
-	M.map('v', lhs, rhs, opts)
+	map('v', lhs, rhs, opts)
 end
 
 function M.xmap(lhs, rhs, opts)
-	M.map('x', lhs, rhs, opts)
+	map('x', lhs, rhs, opts)
 end
 
 function M.tmap(lhs, rhs, opts)
-	M.map('t', lhs, rhs, opts)
+	map('t', lhs, rhs, opts)
 end
 
 
