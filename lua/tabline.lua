4311e789e3c11886fdf9c5b3815255c0210f27cc
Todd Yamakawa <toddy@google.com>
2021-12-22 06:10:42 -0800

    lua/tabline.lua: Using tabnr instead of bufnr.

--- lua/tabline.lua
+++ lua/tabline.lua
@@ -4,15 +4,18 @@
 -- =============================================================================
 -- https://github.com/alvarosevilla95/luatab.nvim/blob/master/lua/luatab/init.lua
 -- https://github.com/toddyamakawa/vim/blob/master/my-plugins/plugin/tabline.vim
+-- https://github.com/akinsho/bufferline.nvim
 
 
 -- =============================================================================
 -- OPTIONS
 -- =============================================================================
+local execute = vim.api.nvim_command
 local fn = vim.fn
 local opt = vim.opt
 
-opt.showtabline = 2
+-- Show if there are at least two tabs
+opt.showtabline = 1
 
 
 function tabline_color(name, ctermfg, ctermbg, guifg, guibg)
@@ -22,9 +25,9 @@ function tabline_color(name, ctermfg, ctermbg, guifg, guibg)
 	local highlight_cmd = 'autocmd VimEnter * '
 		.. 'highlight TabLine'..name
 		.. (ctermfg == nil and '' or ' ctermfg='..ctermfg)
-		.. (ctermbg == nil and '' or ' ctermbg='..ctermbg)
+		.. (ctermbg == nil and ' ctermbg=none' or ' ctermbg='..ctermbg)
 		.. (guifg == nil and '' or ' guifg='..guifg)
-		.. (guibg == nil and '' or ' guibg='..guibg)
+		.. (guibg == nil and ' guibg=none' or ' guibg='..guibg)
 	--vim.cmd('echom "'..highlight_cmd..'"')
 	execute(highlight_cmd)
 end
@@ -36,42 +39,62 @@ local blue    = '%#TabLineBlue#'
 local magenta = '%#TabLineMagenta#'
 local cyan    = '%#TabLineCyan#'
 local white   = '%#TabLineWhite#'
+local other   = '%#Comment#'
 
+local colors = require('colors')
+local cterm = colors.cterm
+local gui = colors.gui
 
-local guibg = 'NONE'
-
--- TODO: Uncommenting this makes everything stop working
---tabline_color('Red'    , 196, 236, '#E080A0', guibg)
-
+tabline_color('Red'    , cterm.red    , nil, gui.red    , nil)
+tabline_color('Green'  , cterm.green  , nil, gui.green  , nil)
+tabline_color('Yellow' , cterm.yellow , nil, gui.yellow , nil)
+tabline_color('Blue'   , cterm.blue   , nil, gui.blue   , nil)
+tabline_color('Magenta', cterm.magenta, nil, gui.magenta, nil)
+tabline_color('Cyan'   , cterm.cyan   , nil, gui.cyan   , nil)
+tabline_color('White'  , cterm.white  , nil, gui.white  , nil)
 
 
 -- =============================================================================
 -- TAB LINE
 -- =============================================================================
 
-function title(bufnr)
-	local bufname = fn.bufname(bufnr)
-	local buftype = fn.getbufvar(bufnr, '&buftype')
+function tabname(tabnr)
+
+	local buflist  = fn.tabpagebuflist(tabnr)
+	local winnr    = fn.tabpagewinnr(tabnr)
+	local bufnr    = buflist[1]
+	local bufname  = fn.bufname(bufnr)
+	local buftype  = fn.getbufvar(bufnr, '&buftype')
 	local filetype = fn.getbufvar(bufnr, '&filetype')
 
-	modified = fn.getbufvar(bufnr, '&modified') == 1 and red..'~' or ''
+	-- TODO: Check for any modified buffer in tab
+	local modified = fn.getbufvar(bufnr, '&modified') == 1 and red..'~' or ''
 
-	return '[' .. bufnr .. ']' .. bufname .. modified
+	local current = tabnr == fn.tabpagenr()
+
+	local tabname = ''
+		.. (current and blue or other)..'['..tabnr..']'
+		.. (current and white or other)..bufname
+		.. modified
+
+	return tabname
 end
 
 
 function tabline()
 	local tabline = ''
 
-	for bufnr = 1, fn.tabpagenr('$'), 1 do
-		tabline = tabline .. title(bufnr) .. ' '
+	tabnrs = fn.tabpagenr('$')
+	--tabline = tabline .. yellow..'['..tabnrs..']  '
+	tabline = tabline .. '%#LineNr#[NVIM] '
+
+	for tabnr = 1, tabnrs, 1 do
+		tabline = tabline .. tabname(tabnr) .. ' '
 	end
 
 
 	return tabline
 end
 
--- REVISIT: Set this when I get this working
---opt.tabline = '%!luaeval("tabline()")'
-
+opt.tabline = '%!luaeval("tabline()")'
 
