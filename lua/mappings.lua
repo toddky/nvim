2a9f581141f9c89dac0eae5af945cbc56e417467
Todd Yamakawa <tyamakawa@tenstorrent.com>
2023-07-11 15:01:51 -0500

    lua/mappings.lua: Moving mappings for gj/gk.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -40,7 +40,12 @@ util.nmap('<C-c>', '<Nop>')
 -- MOVEMENT
 -- =============================================================================
 
--- TODO: Map j to gj with v:count
+-- REVISIT: Use nvmap
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('v', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('v', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Beginning/end of line
 util.nvmap('gh', '^')
