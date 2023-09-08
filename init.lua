55235b1dabcf328bdad0c4cd4b2f09684e27bc52
Todd Yamakawa <tyamakawa@tenstorrent.com>
2023-09-08 14:22:31 -0500

    init.lua: Moving into treesitter config into setup/treesitter.lua.

--- init.lua
+++ init.lua
@@ -360,82 +360,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 	pattern = '*',
 })
 
--- Treesitter Configuraiton
--- See `:help nvim-treesitter`
-require('nvim-treesitter.configs').setup {
-	-- Treesitter languages
-	ensure_installed = {
-		'c',
-		--'cpp',
-		'go',
-		'lua',
-		'python',
-		'rust',
-		--'tsx',
-		--'typescript',
-		--'help',
-		--'vim',
-	},
-	auto_install = false,
-
-	highlight = { enable = true },
-	indent = { enable = true, disable = { 'python' } },
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			init_selection = '<c-space>',
-			node_incremental = '<c-space>',
-			scope_incremental = '<c-s>',
-			node_decremental = '<M-space>',
-		},
-	},
-
-	textobjects = {
-		select = {
-			enable = true,
-			lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-			keymaps = {
-			-- You can use the capture groups defined in textobjects.scm
-			['aa'] = '@parameter.outer',
-			['ia'] = '@parameter.inner',
-			['af'] = '@function.outer',
-			['if'] = '@function.inner',
-			['ac'] = '@class.outer',
-			['ic'] = '@class.inner',
-			},
-		},
-		move = {
-			enable = true,
-			set_jumps = true, -- whether to set jumps in the jumplist
-			goto_next_start = {
-				[']m'] = '@function.outer',
-				[']]'] = '@class.outer',
-			},
-			goto_next_end = {
-				[']M'] = '@function.outer',
-				[']['] = '@class.outer',
-			},
-			goto_previous_start = {
-				['[m'] = '@function.outer',
-				['[['] = '@class.outer',
-			},
-			goto_previous_end = {
-				['[M'] = '@function.outer',
-				['[]'] = '@class.outer',
-			},
-		},
-		swap = {
-			enable = true,
-			swap_next = {
-				['<leader>a'] = '@parameter.inner',
-			},
-			swap_previous = {
-				['<leader>A'] = '@parameter.inner',
-			},
-		},
-	},
-}
-
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
@@ -446,6 +370,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 -- =============================================================================
 -- PLUGIN SETUP
 -- =============================================================================
+require('setup.treesitter')
 require('setup.telescope')
 require('setup.lsp')
 require('setup.cmp')
