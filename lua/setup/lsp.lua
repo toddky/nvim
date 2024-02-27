c14ede2d333cc7040b3e0c4608197d5e94f899da
Todd Yamakawa <tyamakawa@tenstorrent.com>
2024-02-27 15:29:31 -0600

    lua/setup/lsp.lua: Cleaning up mason.

--- lua/setup/lsp.lua
+++ lua/setup/lsp.lua
@@ -77,6 +77,11 @@ require('neodev').setup()
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
+
+-- =============================================================================
+-- MASON
+-- =============================================================================
+
 -- Setup mason so it can manage external tooling
 require('mason').setup()
 
@@ -84,16 +89,16 @@ require('mason').setup()
 local mason_lspconfig = require 'mason-lspconfig'
 
 mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
+	ensure_installed = vim.tbl_keys(servers),
 }
 
 mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    }
-  end,
+	function(server_name)
+	require('lspconfig')[server_name].setup {
+		capabilities = capabilities,
+		on_attach = on_attach,
+		settings = servers[server_name],
+	}
+	end,
 }
 
