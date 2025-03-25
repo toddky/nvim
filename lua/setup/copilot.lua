a65cd5d7ca961f20f5457a92ca5949f10c4296c5
Todd Yamakawa <tyamakawa@tenstorrent.com>
2025-03-25 16:14:30 -0500

    lua/setup/copilot.lua: Updating.

--- lua/setup/copilot.lua
+++ lua/setup/copilot.lua
@@ -9,17 +9,20 @@ end
 vim.api.nvim_create_autocmd('BufEnter', {
 	pattern = 'copilot-*',
 	callback = function()
-		-- Set buffer-local options
+		vim.opt_local.number         = false
 		vim.opt_local.relativenumber = false
-		vim.opt_local.number = false
-		vim.opt_local.conceallevel = 0
+		vim.opt_local.conceallevel   = 0
 
 		--REVISIT: Figure out how to disable bad keymaps
 		--pcall(vim.api.nvim_buf_del_keymap, 0, 'n', ':')
-		--pcall(vim.api.nvim_buf_del_keymap, 0, 'i', '<Enter>')
-
 	end
 })
+
+
+-- =============================================================================
+-- MAPPINGS
+-- =============================================================================
+vim.api.nvim_set_keymap('n', ',cc', '<cmd>CopilotChatOpen<CR>', { noremap = true, silent = true })
 
 
 -- =============================================================================
@@ -31,9 +34,4 @@ vim.api.nvim_create_autocmd('BufEnter', {
 -- SETTINGS
 -- =============================================================================
 
-
--- =============================================================================
--- MAPPINGS
--- =============================================================================
-vim.api.nvim_set_keymap('n', ',cc', '<cmd>CopilotChat<CR>', { noremap = true, silent = true })
 
