d0defc115a2ec2dc217d48e3911f1596051551b0
Todd Yamakawa <tyamakawa@tenstorrent.com>
2025-06-17 17:06:56 -0500

    lua/functions.lua: Add 'Popup' command for displaying git status in a tmux popup window.

--- lua/functions.lua
+++ lua/functions.lua
@@ -7,7 +7,14 @@
 -- =============================================================================
 -- FUNCTIONS
 -- =============================================================================
+
+-- Usage: Open a left and right split then run :Diff
 vim.api.nvim_create_user_command('Diff', function()
 	vim.cmd('windo diffthis')
 end, {})
+
+-- :Popup git status | less
+vim.api.nvim_create_user_command('Popup', function(opts)
+	vim.fn.system({"tmux", "display-popup", "-E", opts.args})
+end, { nargs = 1 })
 
